283ada274788b7d1ea06a784bb3806248957e6d6
Nitesh <niteshprimedev@gmail.com>
2025-01-28 21:42:57 +0530

    Section-4 Form Validation

--- tests/uiBasicsTest.spec.js
+++ tests/uiBasicsTest.spec.js
@@ -24,3 +24,99 @@ async function callbackFunctionTest1({ browser }){
     console.log("All went well and executed step by step i.e synchronously");
 }
 
+// Writing second test case;
+test('Select Playwright Site Heading', selectPlaywrightDevHeading);
+
+//
+async function selectPlaywrightDevHeading({ page }) {
+    // visit page url or website;
+    await page.goto("https://leetcode.com/u/niteshprimedev/");
+
+    console.log('Playwright second test case');
+
+    // Select the Playwright Heading;
+    // const pageEl = await page.locator();
+    const pageElText = await page.textContent(`[class="text-label-1 dark:text-dark-label-1 break-all text-base font-semibold"]`);
+    console.log(pageElText);
+    console.log("Hellow World");
+}
+
+test('Fill Form', cbFillForm);
+
+async function cbFillForm({ page }){
+    // Visit page url or website
+    await page.goto("https://sso.teachable.com/secure/146684/identity/sign_up/otp");
+
+    // Get the page or website title
+    const pageTitle = await page.title();
+
+    await console.log('Page Title', pageTitle);
+
+    // Select the form input with id selector;
+    await page.locator('input#name').fill('Rahul Shetty');
+
+    await page.locator('input#email').fill('rahulshetty@gmail.com');
+
+    await console.log("Test Fill Form is successfully executed");
+
+    const isPageClosed = await page.isClosed();
+
+    await console.log("isPageClosed?", isPageClosed);
+}
+
+test.only('Sec-4 Form Validation', loginFormValidation);
+
+async function loginFormValidation({ page }){
+    const userEmail = page.locator('input#email');
+    const userPasword = page.locator('input#password');
+    const loginBtn = page.locator('data-testid=login-button');
+
+    // visit the page url or website;
+    await page.goto("https://sso.teachable.com/secure/9521/identity/login/password?force=true");
+
+    // Validate the page load with the title;
+    const isPageTitleVerified = await expect(page).toHaveTitle(/Rahul Shetty Academy/);
+    console.log('Page title verified: ', isPageTitleVerified);
+
+    // Select the form input fields;
+    // fill the input with the id = email 
+    await userEmail.fill('rahulshetty@gmail.com');
+
+    // fill the input with the id = password 
+    await userPasword.fill('rahulpassword');
+
+    // show the password by clicking on the eye icon with the id = icon-e
+    await page.locator('svg#icon-e').click();
+    
+    // uncheck the remember me checkbox with the id = remember_me
+    await page.locator('input#remember_me').uncheck();
+
+    await setTimeout(() => {
+        console.log("Should see the test execution");
+    }, 5000);
+
+    await loginBtn.click();
+
+    // await page.locator('data-transition=entered').waitFor();
+    // const errorTxtMsg = await page.locator('data-transition=entered').textContent();
+    // const errorTxtMsg = await page.locator(`[class="bodyMainBold p-h-3-xs p-v-3-xs m-r-4-xs m-b-7-xs dsp-flex-xs flex-align-items-center-xs toast success"]`).innerText();
+    // const errorTxtMsg = await page.locator(`[style="transform:translate3d(0%, 0, 0)"]`).innerText();
+
+    // await console.log('Error Message: ', errorTxtMsg);
+
+    // Assertiont to check it;
+    await expect(page.locator(`[style="transform=translate3d(0, 0, 0)"]`)).toContainText('Incorrect');
+
+    // Clear the inputs with id = email;
+    await userEmail.fill('');
+    
+    // Fill the inputs with the id = email
+    await userEmail.fill('playwright@dev.com');
+
+    // Login the with the id = login;
+    await loginBtn.click();
+
+    await page.goto('');
+}
+
+// test.only('Sec-4 Working with Multiple Elements', )
